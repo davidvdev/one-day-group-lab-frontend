0c205bda8ad9cce6f3113f45c1f5d66d789414ba
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-08-19 13:49:18 -0400

    playlist layout adjusted

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -14,16 +14,18 @@ const Playlist = (props) => {
                         <div className="main-playlist-display">
                             <div className="playlist-line-one">
                                 <h4 className="song-title">{song.title}</h4>
-                                <div className="right-side">
-                                    <h4 className="song-artist">{song.artist}</h4>
-                                    <button className="delete-button" onClick={() => {
-                                        props.deleteSong(song)
-                                    }}>Delete</button>
-                                    <button className="favorite-button">Favorite</button>
-                                </div>
+                                <h4 className="song-artist">{song.artist}</h4>
                             </div>
                             <div className="playlist-line-two">
                                 <h4>{song.time}</h4>
+                                <div className="right-side">
+                                    <button className="delete-button" onClick={() => {
+                                        props.deleteSong(song)
+                                    }}>Delete</button>
+                                    <button className="favorite-button" onClick={() => {
+                                        props.updateFavorites(song)
+                                    }}>Favorite</button>
+                                </div>
                             </div>
                         </div>
                     </article>
